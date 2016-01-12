d1cf4389e64430ef6c42af55bb08e39801ae9718
Wong You Jing <wongyoujing118@gmail.com>
2016-01-11 17:46:21 -0800

    add bolded text

--- WordPlay/ResultsViewController.m
+++ WordPlay/ResultsViewController.m
@@ -25,7 +25,25 @@
 
 - (void)storyMaker
 {
-    self.resultsTextView.text = [NSString stringWithFormat:@"Hi my name is %@ and I'm %@. I work %@ if there is %@ in the room.", self.story.name, self.story.adjective, self.story.adverb, self.story.noun];
+    
+    NSString *fullStory = [NSString stringWithFormat:@"Hi my name is %@ and I'm %@. I work %@ if there is %@ in the room.", self.story.name, self.story.adjective, self.story.adverb, self.story.noun];
+
+    
+    NSRange rangeName = [fullStory rangeOfString:self.story.name];
+    NSRange rangeAdjective = [fullStory rangeOfString:self.story.adjective];
+    NSRange rangeAdverb = [fullStory rangeOfString:self.story.adverb];
+    NSRange rangeNoun = [fullStory rangeOfString:self.story.noun];
+    
+    UIFont *fontText = [UIFont boldSystemFontOfSize:20];
+    NSDictionary *dictBoldText = [NSDictionary dictionaryWithObjectsAndKeys:fontText, NSFontAttributeName, nil];
+    
+    NSMutableAttributedString *mutAttrTextViewString = [[NSMutableAttributedString alloc] initWithString:fullStory];
+    [mutAttrTextViewString setAttributes:dictBoldText range:rangeName];
+    [mutAttrTextViewString addAttributes:dictBoldText range:rangeAdjective];
+    [mutAttrTextViewString addAttributes:dictBoldText range:rangeAdverb];
+    [mutAttrTextViewString addAttributes:dictBoldText range:rangeNoun];
+    
+    [self.resultsTextView setAttributedText:mutAttrTextViewString];
 }
 
 
